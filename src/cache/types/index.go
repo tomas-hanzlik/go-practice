2b32f4c3f0ea475c22ecb20bf984b5c15ffb6fde
tomas-hanzlik <tomas.hanzlik0@gmail.com>
2020-03-03 18:51:14 +0000

    Types: Add CacheItem type with key and value fields

---
+++ src/cache/types/index.go
@@ -0,0 +1,10 @@
+package types
+
+
+// Add CacheItem type with one string field for storing cache value. Put its definition to src/cache/types dir. Add new method called Ad
+// dItem to Cache which would have one argument - of CacheItem type. You will also need to create some container for cache items.
+// AddItem should be concurrent, work with sync.Map or implement your own logic.
+type CacheItem struct {
+	Key string  // To preserve the interface from other tasks (addItem, Adapters, ...)... TOOD: Ask
+	Value string
+}
