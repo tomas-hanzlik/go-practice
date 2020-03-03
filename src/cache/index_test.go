3160b3f375ad6dc1b0c1b83c42664f0e018d1a51
tomas-hanzlik <tomas.hanzlik0@gmail.com>
2020-03-03 20:08:41 +0000

    Cache: clean tests

--- src/cache/index_test.go
+++ src/cache/index_test.go
@@ -3,13 +3,10 @@ package cache
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
-	"fmt"
 	types "./types"
 )
 
-// add global list of key during init
-
-
+// test data
 var defaultKeys = map[string]string{
     "one": "tomas",
     "two": "2138",
@@ -21,6 +18,7 @@ type MockCache struct {
 	*Cache
 }
 
+// Helper function to fill cache with data to make testing of some fucntionality easier
 func (cache *MockCache) FillWithDefaultData() {
 	for key, value := range defaultKeys {
 		cache.AddItem(types.CacheItem{Key: key, Value: value})
@@ -31,19 +29,18 @@ func NewMockCache() *MockCache {
 	return &MockCache{Cache: NewCache()}
 }
 
+// helper function that can abstract out some logic and prepares testing state - in this case it creates basic cache instance
+func prepareBrandNewCache() *MockCache {
+	cache := NewMockCache()
+
+	return cache
+}
 
 // Tests if constructor works
 func TestNewCache(t *testing.T) {
 	cache := NewMockCache()
 
 	assert.NotNil(t, cache, "cache constructor should not return nil")
-}
-
-// helper function that can abstract out some logic and prepares testing state - in this case it creates basic cache instance
-func prepareBrandNewCache() *MockCache {
-	cache := NewMockCache()
-
-	return cache
 }
 
 // unit test for specific method
@@ -53,32 +50,6 @@ func TestCache_Size(t *testing.T) {
 	assert.Empty(t, cache.Size(), "brand new cache should have zero items")
 }
 
-// Test adding of new items into the cache
-func TestCache_AddItem_GetItem(t *testing.T) {
-	// TODO: Split use cases
-	cache := prepareBrandNewCache()
-	// cache.FillWithDefaultData()
-
-	// test AddItem
-	newItem := types.CacheItem{Key: "32", Value: "43"}
-	e := cache.AddItem(newItem)
-	assert.Equal(t, int64(1), cache.Size(), "Cache should have exactly one item.")
-	assert.Nil(t, e, "Failed to add item")
-
-	// test GetItem
-	item, found := cache.GetItem("34")
-	fmt.Println(item, found)
-	assert.False(t, found, "In case of unknown item should return false.")	
-	assert.Empty(t, item, "In case of unknown item should be empty.")
-
-	item, found = cache.GetItem("32")
-	fmt.Println(item, found, defaultKeys)
-	assert.True(t, found, "In case of known item should return true.")	
-	assert.Equal(t, item, newItem, "In case of known item should correct item.")
-
-}
-
-
 func TestCache_AddItem(t *testing.T) {
 	cache := prepareBrandNewCache()
 
@@ -86,8 +57,6 @@ func TestCache_AddItem(t *testing.T) {
 	assert.Equal(t, int64(1), cache.Size(), "Cache should have exactly one item.")
 	assert.Nil(t, e, "Failed to add item")
 }
-
-
 
 func TestCache_GetItem(t *testing.T) {
 	cache := prepareBrandNewCache()
